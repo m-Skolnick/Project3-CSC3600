b4266e02b0b1bf461df2e9cfc67509bd9c4a9cfc
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-02-18 16:24:17 -0600

    Final Edit

--- Project3-CSC3600/CustomerListClass.h
+++ Project3-CSC3600/CustomerListClass.h
@@ -4,10 +4,11 @@
 //*                                                                                                   *
 //*                                                                                                   *
 //*   USER DEFINED                                                                                    *
-//*    MODULES     : insertRecord - Insert one record of type RecordType into list of records         *
-//*                  printRecords - Print each of the records in the list                             *
-//*                  deleteRecord - Delete one record from the list	                                  *
-//*                  changeRecord - Change one value of one record in record list                     *
+//*    MODULES     : add - Insert one record of type CustomerType into list of records                *
+//*                  pop - Remove and return one record from the front of the list                    *
+//*                  checkForPop - Check to see if the front customer is ready to be popped from list *
+//*                  isEmpty - Check to see whether the list is empty                                 *
+//*                  getPTime() - Get the total process time for the list of customers
 //*                                                                                                   *
 //*****************************************************************************************************
 #include <iostream>
@@ -27,7 +28,7 @@ struct CustomerType {
 class CustomerListClass {
 public:
 	CustomerListClass();
-	void push(CustomerType);
+	void add(CustomerType);
 	CustomerType pop();
 	bool checkForPop(int);
 	bool isEmpty();
@@ -41,19 +42,22 @@ CustomerListClass::CustomerListClass() {
 	StartPtr = NULL;
 }
 //*****************************************************************************************************
-
-//*****************************************************************************************************
-
-inline void CustomerListClass::push(CustomerType customer)
+inline void CustomerListClass::add(CustomerType customer)
 {
+		// Receives – One structure of type CustomerType
+		// Task - Add the received structure to the back of customerlist
+		// Returns - Nothing
+
+		// Read in the received customer to a new pointer.
 	CustomerType *CurrentPtr, *newPtr;
 	newPtr = new(CustomerType);
 	newPtr ->tArrival = customer.tArrival;
-	newPtr->tArrival = customer.tArrival;
 	newPtr->name = customer.name;
 	newPtr->tProcess = customer.tProcess;
+
+		
 	CurrentPtr = StartPtr;
-		// If there are no nodes in list, push to start
+		// If there are no nodes in list, put this one at the beginning and return
 	if (StartPtr == NULL) {
 		StartPtr = newPtr;
 		newPtr->next = NULL;
@@ -67,42 +71,62 @@ inline void CustomerListClass::push(CustomerType customer)
 	CurrentPtr ->next = newPtr;
 	newPtr->next = NULL;
 }
+//*****************************************************************************************************
 inline int CustomerListClass::getPTime() {
-
+		// Receives – Nothing
+		// Task - Add up the processing time for the list
+		// Returns - The total processing time for this list
 	CustomerType *CurrentPtr;
 	CurrentPtr = StartPtr;
 	int processTime = 0;
-	if (StartPtr == NULL) {
-		return 0;
-	}
+		// Add the processing time for each node
 	while (CurrentPtr != NULL) {
 		processTime += CurrentPtr->tProcess;
 		CurrentPtr = CurrentPtr->next;
 	}
+		// Return the processing time for the list
 	return processTime;
 }
+//*****************************************************************************************************
 inline CustomerType CustomerListClass::pop()
 {
+		// Receives – Nothing
+		// Task - Remove one node from front of list
+		// Returns - The removed node.
 	CustomerType *TempPtr;
 	TempPtr = StartPtr;
 	StartPtr = StartPtr->next;
 	return *TempPtr;
 }
-
+//*****************************************************************************************************
 inline bool CustomerListClass::checkForPop(int currentTime)
 {
+		// Receives – The current processing time in type int
+		// Task - Check whether the front node of each list is ready to be removed
+		// Returns - true or false whether it is time for first node to be removed
+	int thisPtrTime;
+		// if the start time+process time for the front node of the list is equal to the current time
+		// return true signifying that the list is ready to be popped.
 	if (StartPtr != NULL) {
-		if (currentTime == StartPtr->tProcess + StartPtr->tArrival) {
-			return true;		}		
+		thisPtrTime = StartPtr->tProcess + StartPtr->tArrival;
+		if (currentTime == thisPtrTime) {
+			return true;		
+		}	
+		else {
+			return false;
+		}
 	}
 	else {
 		return false;
 	}
 	
 }
-
+//*****************************************************************************************************
 inline bool CustomerListClass::isEmpty()
 {
+		// Receives – Nothing
+		// Task - Check whether the list is empty
+		// Returns - true or false signifying whether the list is empty or not.
 	if (StartPtr == NULL) {
 		return true;
 	}
--- Project3-CSC3600/Project3.cpp
+++ Project3-CSC3600/Project3.cpp
@@ -1,26 +1,26 @@
 //************************************  PROGRAM IDENTIFICATION  ***************************************
 //*                                                                                                   *
-//*   PROGRAM FILE NAME:  Project2.cpp          ASSIGNMENT #:  2            Grade: _________          *
+//*   PROGRAM FILE NAME:  Project3.cpp          ASSIGNMENT #:  3            Grade: _________          *
 //*                                                                                                   *
 //*   PROGRAM AUTHOR:     __________________________________________                                  *
 //*                                     Micaiah Skolnick                                              *
 //*                                                                                                   *
-//*   COURSE #:  CSC 36000 11                            DUE DATE:  February 6, 2017                  *
+//*   COURSE #:  CSC 36000 11                            DUE DATE:  February 22, 2017                 *
 //*                                                                                                   *
 //*****************************************************************************************************
 //***********************************  PROGRAM DESCRIPTION  *******************************************
 //*                                                                                                   *
-//*   PROCESS: This program first reads a one character code, then depending on the code, processes   *
-//*              values for a record list on the subsequent input lines. Records are stored in a      *
-//*              structure, and the list of structures is built by a class which controls the list.   *
+//*   PROCESS: This program reads each a list of customers, then processes each customer thrugh the   *
+//*                 correct register based on their arrival time to the checkout counter, and their   *
+//*                 designated process time. A list of the order in which customers arrived, and a    *
+//*                 list of the order that customers exited the store is provided to the user.        *
 //*                                                                                                   *
 //*   USER DEFINED                                                                                    *
-//*    MODULES     : newPage - Prints a blank line for each blank space left on the page              *
-//*                  Footer - Prints a footer to signify end of program output                        *
+//*    MODULES     : Footer - Prints a footer to signify end of program output                        *
 //*                  Header - Prints a header to signify start of program output                      *
-//*                  getData - Data from the input file is read into the program                      *
+//*                  processData - Data from the input file is read into the program and processed    *
 //*                  main - Variables are declared, functions are called, and headers are printed     *
-//*                  processData - Read each Op code character and perform the subsequent action.     *
+//*                  printResults - Read each Op code character and perform the subsequent action.     *
 //*                                                                                                   *
 //*****************************************************************************************************
 #include <iostream>
@@ -31,20 +31,6 @@
 #include <sstream>
 #include "CustomerListClass.h"
 using namespace std;
-int lineCount;
-int MAXLINECOUNT;
-//*****************************************************************************************************
-void newPage(ofstream&dataOUT) {
-	// Receives – the output file
-	// Task - Insert blank lines to fill the rest of the current page
-	// Returns - Nothing
-	while (lineCount < MAXLINECOUNT) {
-		dataOUT << endl;
-		lineCount++;
-	}
-	lineCount = 0; // Reset the line count to 0 for next page.
-}
-//*****************************************************************************************************
 //*************************************  FUNCTION HEADER  *********************************************
 void Header(ofstream &Outfile)
 {       // Receives – the output file
@@ -54,15 +40,14 @@ void Header(ofstream &Outfile)
 	Outfile << setw(17) << "CSC 36000";
 	Outfile << setw(15) << "Section 11" << endl;
 	Outfile << setw(27) << "Spring 2017";
-	Outfile << setw(30) << "Assignment #2" << endl;
+	Outfile << setw(30) << "Assignment #3" << endl;
 	Outfile << setw(35) << "---------------------------------- - ";
 	Outfile << setw(35) << "---------------------------------- - " << endl << endl;
-	lineCount += 4;
 	return;
 }
 //************************************* END OF FUNCTION HEADER  ***************************************
 //*************************************  FUNCTION FOOTER  *********************************************
-void Footer(ofstream &Outfile, int lineCount)
+void Footer(ofstream &Outfile)
 {
 		// Receives – the output file
 		// Task - Prints the output salutation
@@ -71,89 +56,101 @@ void Footer(ofstream &Outfile, int lineCount)
 	Outfile << setw(35) << "-------------------------------- - " << endl;
 	Outfile << setw(35) << " | END OF PROGRAM OUTPUT | " << endl;
 	Outfile << setw(35) << "-------------------------------- - " << endl;
-	lineCount += 4;
 	return;
 }
 //************************************* END OF FUNCTION FOOTER  ***************************************
 void checkForPops(CustomerListClass&pushedList, CustomerListClass&poppedList,CustomerListClass&List1,
 	CustomerListClass&List2, CustomerListClass&List3, int currentTime) {
+		// Receives – List of input order, list of output order, each customer queue, and the current
+			// time
+		// Task - Check whether a node in each list is ready to be popped, if so, pop it.
+		// Returns - Nothing
+	int time = 0;
+		// Check whether a customer should be popped from any of the lists.
+	while (time <= currentTime) {
 
-	if (List1.checkForPop(currentTime)) {
-		poppedList.push(List1.pop());
+		if (List1.checkForPop(time)) {
+			poppedList.add(List1.pop());
+		}
+		if (List2.checkForPop(time)) {
+			poppedList.add(List2.pop());
+		}
+		if (List3.checkForPop(time)) {
+			poppedList.add(List3.pop());
+		}
+		time++;
 	}
-	else if (List2.checkForPop(currentTime)) {
-		poppedList.push(List2.pop());
-	}
-	else if (List3.checkForPop(currentTime)) {
-		poppedList.push(List3.pop());
-	}
-
 }
-void processData(ifstream&dataIN,CustomerListClass&pushedList,CustomerListClass&poppedList) {
+//*****************************************************************************************************
+void processData(ifstream&dataIN,CustomerListClass&arrivalList,CustomerListClass&departureList) {
 		// Receives – The input and output files
 		// Task - Process each data record/op code.
 		// Returns - Nothing
 	
-	int currentTime = -1;
+		// Declare local variables
+	int currentTime = 0;
 	CustomerListClass List1, List2, List3;
 	bool firstPrint = true;
-
 	CustomerType customer;
 	int fieldNumber;
 	string newValue;
-
 	int code;
 	dataIN >> ws >> code >> ws; // seed read first code
-
 	while (code != -99) {  // As long as there is another code, read it in
 		customer.tArrival = code;
 		std::getline(dataIN, customer.name);
 		dataIN >> customer.tProcess;
-
-
-		while (currentTime != customer.tArrival) {
-			currentTime++;
-
-			checkForPops(pushedList, poppedList, List1, List2, List3, currentTime);
-
+			// Loop until the time of the next customer
+		while (currentTime <= customer.tArrival) {
+				// When the next customer arrival time is reached, add them to a list, and to the 
+					// arrival order list.
 			if (currentTime == customer.tArrival) {
 				if (List1.getPTime() <= List2.getPTime() && List1.getPTime() <= List3.getPTime()) {
-					List1.push(customer);
+					List1.add(customer);
 				}
 				else if (List2.getPTime() <= List3.getPTime()) {
-					List2.push(customer);
+					List2.add(customer);
 				}
 				else {
-					List3.push(customer);
+					List3.add(customer);
 				}
-				pushedList.push(customer);
-			}
-			
-		}		
+				arrivalList.add(customer);
+			}	
+				// Check whether any of the customers are ready to leave the store.
+			checkForPops(arrivalList, departureList, List1, List2, List3, currentTime);			
+			currentTime++;
+		}
 		dataIN >> ws >> code >> ws; // Read in the next code
+	}
+		// Check one more time to make sure all customers have left the store
+	currentTime = 0;
+	while (!List1.isEmpty() || !List2.isEmpty() || !List3.isEmpty()) {
+		checkForPops(arrivalList, departureList, List1, List2, List3, currentTime);
+		currentTime++;	
 	}
 }
 //*****************************************************************************************************
 void printResults(ofstream&dataOUT,CustomerListClass&pushedList,CustomerListClass&poppedList) {
-	
+		// Receives – The output file, order of customer arrival, and exit.
+		// Task - Print each list of customers
+		// Returns - Nothing	
+	dataOUT << "The order of customer arrival is:    The order of customer departure is:" << endl;
+	dataOUT << "--------------------------------- |  -----------------------------------" << endl;
 	while (!pushedList.isEmpty()&&!poppedList.isEmpty()) {
 		dataOUT << pushedList.pop().name;
-		dataOUT << "    ";
+		dataOUT << "              |   ";
 		dataOUT << poppedList.pop().name << endl;
 	}
-
 }
 //*****************************************************************************************************
 int main() {
-	// Receives – Nothing
-	// Task - Call each necessary function of the program in order
-	// Returns - Nothing
+		// Receives – Nothing
+		// Task - Call each necessary function of the program in order
+		// Returns - Nothing
 	// Declare variables used in program.	 
 	ifstream dataIN;
 	ofstream dataOUT;
 	CustomerListClass pushedList, poppedList;
-	lineCount = 0;
-	MAXLINECOUNT = 54;
 
 	dataIN.open("queue_in.txt"); // Open the file containing data.
 	dataOUT.open("dataOUT.doc"); // Create and open the file to write data to.		
@@ -161,10 +158,8 @@ int main() {
 	//receiveData(dataIN, pushedList, poppedList);
 	processData(dataIN,pushedList,poppedList); // Process each section of data from the input file.
 	printResults(dataOUT, pushedList, poppedList);
-	newPage(dataOUT); // Insert a page break before the footer
-	Footer(dataOUT, lineCount); // Print footer. 
+	Footer(dataOUT); // Print footer. 
 	dataIN.close(); // Close input data file. 
 	dataOUT.close(); // Close output data file.
-
 	return 0;
 }
